74f6b8a329356c7724d4ce89da3ee3c1e270cf03
Angad Singh <mail@angad.dev>
2022-09-24 11:30:14 +0530

    chore: setup eslint, editorconfig to switch tabs to spaces

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'sort-keys': ['error', 'asc', { caseSensitive: true, natural: false }],
+  },
+}
--- src/index.js
+++ src/index.js
@@ -8,44 +8,45 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 // export interface Env {
-// 	AIRTABLE_API_KEY: string;
-// 	AIRTABLE_BASE_ID: string;
-// 	AIRTABLE_TABLE_NAME: string;
-// 	MAIN_DOMAIN: string;
-// 	GITHUB_USERNAME: string;
+//  AIRTABLE_API_KEY: string;
+//  AIRTABLE_BASE_ID: string;
+//  AIRTABLE_TABLE_NAME: string;
+//  MAIN_DOMAIN: string;
+//  GITHUB_USERNAME: string;
 // }
 
 export default {
-	async fetch(
-		request,
-		env,
-		_ctx
-	) {
-		const pathname = new URL(request.url).pathname;
+  async fetch(
+    request,
+    env,
+    _ctx
+  ) {
+    const pathname = new URL(request.url).pathname;
 
-		// Handle main domain redirect
-		if (pathname === "/") {
-			return Response.redirect(`https://${env.MAIN_DOMAIN}`, 302);
-		}
+    // Handle main domain redirect
+    if (pathname === "/") {
+      return Response.redirect(`https://${env.MAIN_DOMAIN}`, 302);
+    }
 
-		// Handle /gh
-		if (pathname.startsWith("/gh")) {
-			return Response.redirect(`https://github.com/${env.GITHUB_USERNAME}/${pathname.slice(4)}`, 302)
-		}
 
-		// Fetch from airtable
-		const { records } = await (
-			await fetch(`https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_TABLE_NAME}?maxRecords=1&filterByFormula=${encodeURIComponent("{slug} = '" + pathname.slice(1) + "'")}`, {
-				headers: new Headers({
-					'Authorization': `Bearer ${env.AIRTABLE_API_KEY}`,
-				}),
-			}))
-			.json()
+    // Handle /gh
+    if (pathname.startsWith("/gh")) {
+      return Response.redirect(`https://github.com/${env.GITHUB_USERNAME}/${pathname.slice(4)}`, 302)
+    }
 
-		if (records.length === 0) {
-			return new Response("Not found", { status: 404 })
-		}
+    // Fetch from airtable
+    const { records } = await (
+      await fetch(`https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_TABLE_NAME}?maxRecords=1&filterByFormula=${encodeURIComponent("{slug} = '" + pathname.slice(1) + "'")}`, {
+        headers: new Headers({
+          'Authorization': `Bearer ${env.AIRTABLE_API_KEY}`,
+        }),
+      }))
+      .json()
 
-		return Response.redirect(records[0].fields.target, 302)
-	}
+    if (records.length === 0) {
+      return new Response("Not found", { status: 404 })
+    }
+
+    return Response.redirect(records[0].fields.target, 302)
+  }
 }
